d299f06bf01967725fdef2a931e904f599bfd507
Tiago Avila <tiago.avila09@gmail.com>
2025-12-05 17:33:01 -0300

    implemented daa - had to use AI though. The table in the docs is really confusing

--- src/gameboy_core/cpu_instructions/cpu_miscellaneous_instructions.rs
+++ src/gameboy_core/cpu_instructions/cpu_miscellaneous_instructions.rs
@@ -1,6 +1,4 @@
 pub trait CpuMiscellaneousInstructions {
-    fn daa_sub(&mut self, bits_4_7: u8, bits_0_3: u8, h_flag: bool, c_flag: bool);
-    fn daa_add(&mut self, bits_4_7: u8, bits_0_3: u8, h_flag: bool, c_flag: bool);
     fn daa(&mut self);
     fn cpl(&mut self);
     fn ei(&mut self);
@@ -31,118 +29,42 @@ impl CpuMiscellaneousInstructions for crate::gameboy_core::cpu::Cpu {
         self.registers.flags.n = false;
     }
 
-    // CORRECT IMPLEMENTATION
+    /// Adjusts register A to form a correct BCD representation after a binary addition or subtraction.
+    /// The adjustment is based on the values of the N, H, and C flags.
+    /// If the previous operation was an addition (N flag is reset):
+    /// - If the H flag is set or the lower nibble of A is greater than 9, add 0x06 to A.
+    /// - If the C flag is set or A is greater than 0x99, add 0x60 to A and set the C flag.
+    /// If the previous operation was a subtraction (N flag is set):
+    /// - If the H flag is set, subtract 0x06 from A.
+    /// - If the C flag is set, subtract 0x60 from A.
+    /// After the adjustment, the Z flag is set if A is zero, and the H flag is cleared.
     fn daa(&mut self) {
-        let mut adjustment = 0;
-        let mut set_carry = false;
-
+        let mut adjustment = 0u8;
+        
         if !self.registers.flags.n {
-            // After addition
-            if self.registers.flags.h || (self.registers.a & 0x0F) > 9 {
+            // After addition (ADD, ADC, INC)
+            if self.registers.flags.h || (self.registers.a & 0x0F) > 0x09 {
                 adjustment += 0x06;
             }
             if self.registers.flags.c || self.registers.a > 0x99 {
                 adjustment += 0x60;
-                set_carry = true;
+                self.registers.flags.set_c_flag(true);
             }
             self.registers.a = self.registers.a.wrapping_add(adjustment);
         } else {
-            // After subtraction
+            // After subtraction (SUB, SBC, DEC)
             if self.registers.flags.h {
-                adjustment += 0x06;
+                adjustment = (adjustment as i16 - 0x06) as u8;
             }
             if self.registers.flags.c {
-                adjustment += 0x60;
+                adjustment = (adjustment as i16 - 0x60) as u8;
+                self.registers.flags.set_c_flag(true);
             }
-            self.registers.a = self.registers.a.wrapping_sub(adjustment);
+            self.registers.a = self.registers.a.wrapping_add(adjustment);
         }
-
+        
         self.registers.flags.set_z_flag_from_u8(self.registers.a);
-        self.registers.flags.set_h_flag(false); // Always cleared
-        self.registers.flags.set_c_flag(set_carry);
-    }
-
-    // fn daa(&mut self) {
-    //     let bits_4_7 = (self.registers.a & 0xF0) >> 4;
-    //     let bits_0_3 = self.registers.a & 0x0F;
-    //     let h_flag = self.registers.flags.h;
-    //     let c_flag = self.registers.flags.c;
-
-    //     if !self.registers.flags.n {
-    //         self.daa_add(bits_4_7, bits_0_3, h_flag, c_flag);
-    //     } else {
-    //         self.daa_sub(bits_4_7, bits_0_3, h_flag, c_flag);
-    //     }
-    // }
-
-    fn daa_add(&mut self, bits_4_7: u8, bits_0_3: u8, h_flag: bool, mut c_flag: bool) {
-        let mut adjustment: u8 = 0;
-
-        if !self.registers.flags.c {
-            if h_flag && bits_0_3 <= 0x04 {
-                if bits_4_7 <= 0x09 {
-                    adjustment += 0x06;
-                } else {
-                    adjustment += 0x66;
-                    c_flag = true;
-                }
-            }
-
-            if !h_flag {
-                if bits_4_7 <= 0x08 && bits_0_3 >= 0xA {
-                    adjustment += 0x06;
-                } else {
-                    if bits_0_3 <= 0x09 {
-                        adjustment += 0x60;
-                        c_flag = true;
-                    } else {
-                        adjustment += 0x66;
-                        c_flag = true;
-                    }
-                }
-            }
-        } else {
-            if h_flag && bits_4_7 <= 0x03 && bits_0_3 <= 0x03 {
-                adjustment += 0x66;
-                c_flag = true;
-            } else {
-                if bits_4_7 <= 0x02 {
-                    if bits_0_3 <= 0x09 {
-                        adjustment += 0x60;
-                        c_flag = true;
-                    } else {
-                        adjustment += 0x66;
-                        c_flag = true;
-                    }
-                }
-            }
-        }
-
-        self.registers.a = self.registers.a.wrapping_add(adjustment);
-        self.registers.flags.set_c_flag(c_flag);
-        self.registers.flags.set_h_flag(false);
-        self.registers.flags.set_z_flag_from_u8(self.registers.a);
-    }
-
-    fn daa_sub(&mut self, bits_4_7: u8, bits_0_3: u8, h_flag: bool, mut c_flag: bool) {
-        let mut adjustment: u8 = 0;
-
-        if c_flag {
-            if h_flag && bits_4_7 >= 0x06 && bits_0_3 >= 0x06 {
-                adjustment = 0x9A;
-                c_flag = true;
-            } else if bits_4_7 >= 0x07 && bits_0_3 <= 0x09 {
-                adjustment = 0xA0;
-                c_flag = true;
-            }
-        } else if h_flag && bits_4_7 <= 0x08 && bits_0_3 >= 0x06 {
-            adjustment = 0xFA;
-        }
-
-        self.registers.a = self.registers.a.wrapping_sub(adjustment);
-        self.registers.flags.set_c_flag(c_flag);
-        self.registers.flags.set_h_flag(false);
-        self.registers.flags.set_z_flag_from_u8(self.registers.a);
+        self.registers.flags.set_h_flag(false); // H flag always cleared after DAA
     }
 
     fn cpl(&mut self) {
